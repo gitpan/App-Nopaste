9a7b0fefd13d65eafea9878eca759022e8c9a4da
Shawn M Moore <cpan@sartak.org>
2009-06-18 21:22:22 +0000

    Import of SARTAK/App-Nopaste-0.11 from CPAN.
    gitpan-cpan-distribution: App-Nopaste
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         SARTAK/App-Nopaste-0.11.tar.gz
    gitpan-cpan-author:       SARTAK
    gitpan-cpan-maturity:     released

--- lib/App/Nopaste.pm
+++ lib/App/Nopaste.pm
@@ -6,7 +6,7 @@ use Module::Pluggable search_path => 'App::Nopaste::Service';
 use base 'Exporter';
 our @EXPORT_OK = 'nopaste';
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 sub nopaste {
     # process arguments {{{
@@ -159,17 +159,11 @@ L<WebService::NoPaste>, L<WWW::PastebinCom::Create>, L<WWW::Rafb::Create>, L<Dev
 
 =head1 AUTHOR
 
-Shawn M Moore, C<< <sartak at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs through RT: email
-C<bug-app-nopaste at rt.cpan.org>, or browse
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Nopaste>.
+Shawn M Moore, C<sartak@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008 Shawn M Moore.
+Copyright 2008-2009 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
